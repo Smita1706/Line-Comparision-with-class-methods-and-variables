2c0319b7f91d714d698136ca77657a6b75ae486b
Smita1706 <smitachinchkar1998@gmail.com>
2022-02-06 21:36:32 +0530

    Refactored : Intsead of class methods and variables global variables and global variables

--- LineComparision_UC2.java
+++ LineComparision_UC2.java
@@ -3,11 +3,10 @@ package com.bridgelabz.LineComparision;
 import java.util.Scanner;
 
 public class LineComparision_UC2 {
-	static int x1, x2;
-	static int y1, y2;
-	static Double Length1, Length2;
+	int x1, x2;
+	int y1, y2;
 
-	public void getInput() {
+	public int getInput() {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter the first co-ordinate  : ");
 		x1 = s.nextInt();
@@ -17,25 +16,29 @@ public class LineComparision_UC2 {
 		x2 = s.nextInt();
 		System.out.println("Enter the second co-ordinate : ");
 		y2 = s.nextInt();
+		return 0;
 	}
-	
-	public double FindLength() {
+
+	public double findLength() {
 		return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 	}
 
+	public boolean checkEquality(Double length1, Double length2){
+		return length1.equals(length2);
+	}
 	public static void main(String [] args){
-
+	 Double length1, length2;
 		LineComparision_UC2 u = new LineComparision_UC2();
-	
-		u.getInput();
-		Length1 = u.FindLength();
-		u.getInput();
-		Length2 = u.FindLength();
 
-		System.out.println("Length of first two points : " + Length1);
-		System.out.println("Length of second two points : " + Length2);
+		u.getInput();
+		length1 = u.findLength();
+		u.getInput();
+		length2 = u.findLength();
 
-		boolean res = Length1.equals(Length2);
+		System.out.println("Length of first two points : " + length1);
+		System.out.println("Length of second two points : " + length2);
+
+		boolean res = u.checkEquality(length1, length2);
 		if(res == true)
 			System.out.println("Both the lengths are equal");
 		else
